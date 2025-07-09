6b9b799221a93a005f17c66f28e9b274d2c90dfa
PauloCCB <paulo.napasa3@gmail.com>
2025-07-09 17:27:15 -0500

    feat: comments unnecesary code on Tracking.tsx

--- src/pages/Tracking.tsx
+++ src/pages/Tracking.tsx
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from "react";
+import  { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { MapPinned, Truck, Package, Clock, Eye } from "lucide-react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { Badge } from "@/components/ui/badge";
 import { DataTable } from "@/components/table/data-table";
 import { useGetShipments } from "@/hooks/use-shipments";
 import { getShipmentColumns } from "@/components/table/columns-shipments";
